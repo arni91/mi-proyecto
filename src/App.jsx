20979553a7fa4f872e459746d08820e4228397c0
arni91 <arnaumarifrances@gmail.com>
2025-09-08 17:16:40 +0200

    proyecto recuperado y actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
-import CancionesAnalytics from "./components/CancionesAnalytics/CancionesAnalytics.jsx";
-import Hobbies from "./components/Hobbies/Hobbies.jsx";
-import "./App.css";
+import './App.css'
+import miFoto from './assets/miFoto.jpg'
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  let variable = 20;
+
   return (
-    <main>
-      <h1>Mi música y yo</h1>
-      <CancionesAnalytics />
-      <Hobbies />
+    <main className="container">
+      <h1>Arnau</h1>
+      <h2>Marí</h2>
+      <p>Que sirva de descripción</p>
+
+      <h2>Aficiones:</h2>
+      <ol>
+        <li>Música</li>
+        <li>Deporte</li>
+        <li>Juegos de rol / mesa</li>
+      </ol>
+
+      {/* Imagen personal */}
+      <img src={miFoto} alt="Foto personal" width="200" />
+
+      {/* Enlace */}
+      <p>
+        Visita <a href="https://www.google.com" target="_blank" rel="noreferrer">Google</a>
+      </p>
+
+      {/* Ejemplo de variable */}
+      <p>Inyectar el valor de una variable = {variable}</p>
     </main>
   );
 }
